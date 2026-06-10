722b2d158696756cf9a96896628372677d205198
Raul <raulsp9007@gmail.com>
2026-06-10 11:15:24 -0400

    chore(sw): bump cache to v1.6 after column reorder merge

--- cashmap_sw.js
+++ cashmap_sw.js
@@ -4,7 +4,7 @@
  * Coloca este archivo en la misma carpeta que index.html
  */
 
-const CACHE_NAME = 'cashmap-v1.5';
+const CACHE_NAME = 'cashmap-v1.6';
 const APP_SHELL  = [
   './',
   './index.html',
