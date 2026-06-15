685355bd068b27fadc00929b391d4904c3ae4825
Raul <raulsp9007@gmail.com>
2026-06-15 19:58:23 -0400

    feat(attach): miniatura de imagen en modal + lightbox al clic

--- cashmap_sw.js
+++ cashmap_sw.js
@@ -4,7 +4,7 @@
  * Coloca este archivo en la misma carpeta que index.html
  */
 
-const CACHE_NAME = 'cashmap-v1.19';
+const CACHE_NAME = 'cashmap-v1.20';
 const APP_SHELL  = [
   './',
   './index.html',
